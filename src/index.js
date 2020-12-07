ad151e93245af48d90d06416527c26280708deae
“Luke” <novelview9@gmail.com>
2020-12-07 16:37:40 +0900

    Add inner drawing area

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 const canvasEvent = function () {
     const canvas = document.getElementById("canvasOne");
     const context = canvas.getContext("2d");
-    context.strokeSTyle = "black";
+    context.fillStyle = "black";
+    context.fillRect(10, 10, 200, 200);
+    context.save();
+    context.rect(0, 0, 50, 50);
+    context.clip();
 
     context.beginPath();
-    context.strokeStyle = "black";
+    context.strokeStyle = "red";
     context.lineWidth = 5;
-    context.moveTo(100, 0);
-    context.bezierCurveTo(0, 495, 300, 55, 150, 300);
-    context.stroke();
+    context.arc(100, 100, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
 
+    context.stroke();
+    context.closePath();
+    context.restore();
+
+    context.beginPath();
+    context.rect(0, 0, 500, 500);
+    context.clip();
+
+    context.beginPath();
+    context.strokeStyle = "blue";
+    context.lineWidth = 5;
+    context.arc(100, 100, 50, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
+
+    context.stroke();
     context.closePath();
 };
 window.addEventListener("load", canvasEvent, false);
