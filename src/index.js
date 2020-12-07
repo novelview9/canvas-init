040b6209cf7a16399b8b98bffeda174e45f09566
“Luke” <novelview9@gmail.com>
2020-12-07 17:18:08 +0900

    Add overlap rect

--- src/index.js
+++ src/index.js
@@ -3,29 +3,14 @@ const canvasEvent = function () {
     const context = canvas.getContext("2d");
     context.fillStyle = "black";
     context.fillRect(10, 10, 200, 200);
-    context.save();
-    context.rect(0, 0, 50, 50);
-    context.clip();
-
-    context.beginPath();
-    context.strokeStyle = "red";
-    context.lineWidth = 5;
-    context.arc(100, 100, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
-
-    context.stroke();
-    context.closePath();
-    context.restore();
-
-    context.beginPath();
-    context.rect(0, 0, 500, 500);
-    context.clip();
-
-    context.beginPath();
-    context.strokeStyle = "blue";
-    context.lineWidth = 5;
-    context.arc(100, 100, 50, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
-
-    context.stroke();
-    context.closePath();
+    context.fillStyle = "red";
+    context.fillRect(1, 1, 50, 50);
+    context.globalCompositeOperation = "source-over";
+    context.fillRect(60, 1, 50, 50);
+    context.globalCompositeOperation = "destination-over";
+    context.fillRect(1, 60, 50, 50);
+    context.globalAlpha = 0.5;
+    context.globalCompositeOperation = "source-atop";
+    context.fillRect(60, 60, 50, 50);
 };
 window.addEventListener("load", canvasEvent, false);
