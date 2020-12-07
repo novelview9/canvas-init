96e6bf8d1762af32524d83c7674c3cb1f471e9b6
“Luke” <novelview9@gmail.com>
2020-12-07 15:44:22 +0900

    Add drawing circle line

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-import danceImage from "./dance.gif";
-
 const canvasEvent = function () {
     const canvas = document.getElementById("canvasOne");
     const context = canvas.getContext("2d");
     context.strokeSTyle = "black";
-    context.lineWidth = 10;
-    context.lineJoin = "bevel";
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.lineTo(25, 0);
-    context.lineTo(25, 25);
-    context.stroke();
-    context.closePath();
 
     context.beginPath();
-    context.moveTo(10, 50);
-    context.lineTo(30, 50);
-    context.lineTo(30, 75);
-    context.stroke();
-    context.closePath();
-
-    context.lineJoin = "round";
-    context.lineCap = "butt";
-    context.beginPath();
-    context.moveTo(10, 100);
-    context.lineTo(35, 100);
-    context.lineTo(35, 125);
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    context.arc(100, 100, 20, (Math.PI / 180) * 0, (Math.PI / 180) * 90, true);
     context.stroke();
     context.closePath();
 };
